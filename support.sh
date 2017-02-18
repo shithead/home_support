b17bf89e7703a305d5f51e3e716d1134a0722107
PwnyTail <pwnytail@c3d2.de>
2017-02-18 21:38:38 +0100

    fix problems
    * rename variable
    * fix file name
    * add sshd options

--- support.sh
+++ support.sh
@@ -10,7 +10,7 @@ AUTHORIZEDKEYFILE="${HOME}/.ssh/authorized_key"
 SSHDPORT=""
 SUPPORTERADDR=""
 SUPPORTERSSHDPORT=""
-SUPPORTERREMOTEPORT=""
+SUPPORTERREMOTESSHPORT=""
 # the public key needs the word supporterkey in the comment.
 export SUPPORTERPUBLICKEY=" supporterkey"
 
@@ -27,20 +27,25 @@ create_keyfile () {
 
 authoriz_supporter () {
 	sudo 	mkdir ${HOME}/.ssh 2>/dev/null; \
-		echo ${SUPPORTERPUBLICKEY} >> ${HOME}/.ssh/authorized_key
+		echo ${SUPPORTERPUBLICKEY} >> ${HOME}/.ssh/authorized_keys
 }
 
 start_sshd () {
-	${SSHDCMD} -o "Port ${SSHDPORT}" -o "PidFile ${SSHDPIDFILE}" -o "PermitRootLogin without-password"
+	${SSHDCMD} \
+		-o "Port ${SSHDPORT}" \
+		-o "PidFile ${SSHDPIDFILE}" \
+		-o "PermitRootLogin without-password" \
+		-o "PasswordAuthentication no" \
+                -o "AllowUsers root"
 }
 
 stop_sshd () {
-	 kill -n 9 $(cat ${SSHDPIDFILE})
+	 kill -9 $(cat ${SSHDPIDFILE})
 	 rm ${SSHDPIDFILE}
 }
 
 establish_tunnel () {
-	${SSHCMD} -N -R ${SUPPORTERREMOTEPORT}:localhost:${SSHDPORT} -i ${SSHDKEYFILE}  ${SSHUSER}@${SUPPORTERADDR} -p${SUPPORTERSSHDPORT}
+	${SSHCMD} -N -R ${SUPPORTERREMOTESSHPORT}:localhost:${SSHDPORT} -i ${SSHDKEYFILE}  ${SSHUSER}@${SUPPORTERADDR} -p${SUPPORTERSSHDPORT}
 }
 
 main () {
@@ -57,8 +62,16 @@ main () {
 		echo -e "Authoriz Supporter"
 		authoriz_supporter
 	fi
+        if [ -e ${SSHDPIDFILE} ]
+        then
+                stop_sshd
+        fi
 	start_sshd
 	establish_tunnel
+	if [ -e ${SSHDPIDFILE} ]
+	then
+		stop_sshd
+	fi
 }
 
 main
